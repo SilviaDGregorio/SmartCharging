9f4d73ad531a26d86f6761a70395e4e10d4404b8
Silvia De Gregorio <silvia@silvias-air.kpn>
2021-07-25 10:38:15 +0200

    init

---
+++ src/SmartCharging.Api/Controllers/GroupController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SmartCharging.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GroupController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IEnumerable<Group>> Get()
+        {
+            return new List<Group>();
+        }
+    }
+}
---
+++ src/SmartCharging.Api/Group.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace SmartCharging.Api
+{
+    public class Group
+    {
+        public string Name { get; set; }
+        public float Amps { get; set; }
+    }
+}
